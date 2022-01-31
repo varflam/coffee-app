7fd75bb08147f1701228ba9b2ff62371d4f20804
Maria <alece.1414@gmail.com>
2022-01-31 19:25:59 +0300

    smooth scroll aded

--- src/features/navigation/Navigation.js
+++ src/features/navigation/Navigation.js
@@ -5,13 +5,20 @@ import './navigation.sass';
 import coffeeIcon from '../../assets/coffee_icon.svg';
 
 const Navigation = ({footer}) => {
+    const smoothScroll = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
     if(footer) {
         return(
             <footer className='footer'>
             <nav className="footer__menu">
-                <Link to="/" className="menu__item footer__menu__item">Coffee house</Link>
-                <Link to="/coffee" className="menu__item footer__menu__item">Our coffee</Link>
-                <Link to="/pleasure" className="menu__item footer__menu__item">For your pleasure</Link>
+                <Link to="/" onClick={smoothScroll} className="menu__item footer__menu__item">Coffee house</Link>
+                <Link to="/coffee" onClick={smoothScroll} className="menu__item footer__menu__item">Our coffee</Link>
+                <Link to="/pleasure" onClick={smoothScroll} className="menu__item footer__menu__item">For your pleasure</Link>
             </nav>
             <div className="lines-element"></div>
             <img src={coffeeIcon} alt="Coffee icon" className="icon" />
