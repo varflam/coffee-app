658995239c97b2dddaa61526adb8df7e7774ea83
Maria <alece.1414@gmail.com>
2022-03-13 14:06:55 +0300

    still trying

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const coffeeApiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001'
+        baseUrl: 'http://localhost:3000'
     }),
     endpoints: builder => ({
         getCoffee: builder.query({
