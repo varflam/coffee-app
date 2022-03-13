545a769eb9d1a270d1a1ec008641c9310fe8d95b
Maria <alece.1414@gmail.com>
2022-03-13 14:31:38 +0300

    server.js added

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const coffeeApiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000'
+        baseUrl: 'https://coffee-app-varflam.herokuapp.com/api'
     }),
     endpoints: builder => ({
         getCoffee: builder.query({
